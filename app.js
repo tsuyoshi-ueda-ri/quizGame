1ad871397282c71b86b97e0163aba9bf6eb01dc8
上田　剛史 <uedatsuyoshi@uedatakeshinoMacBook-Air.local>
2021-02-26 00:35:14 +0900

    Reflect a constant character string in HTML
    # What
    定数をHTMLに反映させる。
    
    # Why
    HTMLに反映させる事により、定数を切替、HTMLに表示される内容も切替え、問題を切り替える。

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const question = '三大旨味成分と言われているのはグアニル酸、イノシン酸、もう一つは次のうちどれか？';
-const answer = [
+const answers = [
   'アミノ酸',
   'コハク酸',
   'アスパラギン酸',
@@ -7,4 +7,14 @@ const answer = [
 ];
 const correct = 'グルタミン酸';
 
+//定数の文字列をHTMLに反映させる
 document.getElementById('js-question').textContent = question;
+document.getElementsByTagName('button')[0].textContent = answers[0];
+document.getElementsByTagName('button')[1].textContent = answers[1];
+document.getElementsByTagName('button')[2].textContent = answers[2];
+document.getElementsByTagName('button')[3].textContent = answers[3];
+
+//ボタンをクリックしたら正誤判定
+document.getElementsByTagName('button')[0].addEventListener('click', () => {
+
+});
