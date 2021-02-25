3a2af93473a7b0daa151cffa6ba38715c4b9bf03
上田　剛史 <uedatsuyoshi@uedatakeshinoMacBook-Air.local>
2021-02-26 06:47:45 +0900

    Refactoring
    # What
    繰り返し使用していた$button[0].textContent = answers[0];の処理をリファクタリング。
    
    # Why
    コードの可読性を上げ、コードに柔軟性を持たせる為にwhileにして繰り返す。

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ document.getElementById('js-question').textContent = question;
 
 const $button = document.getElementsByTagName('button');
 
-$button[0].textContent = answers[0];
-$button[1].textContent = answers[1];
-$button[2].textContent = answers[2];
-$button[3].textContent = answers[3];
-
+let buttonIndex = 0;
+let buttonLength = $button.length;
+while(buttonIndex < buttonLength) {
+  $button[buttonIndex].textContent = answers[buttonIndex];
+  buttonIndex++;
+}
 //ボタンをクリックしたら正誤判定
 document.getElementsByTagName('button')[0].addEventListener('click', () => {
   if(correct === $button[0].textContent){
