6931b0db793f764f81ebb4b4116531274701d6d9
上田　剛史 <uedatsuyoshi@uedatakeshinoMacBook-Air.local>
2021-02-25 23:27:02 +0900

    Make a quiz game
    # What
    JavaScriptをメインにクイズゲームを作る。
    
    # Why
    自分の技術の幅を広げる。

---
+++ app.js
@@ -0,0 +1,10 @@
+const question = '三大旨味成分と言われているのはグアニル酸、イノシン酸、もう一つは次のうちどれか？';
+const answer = [
+  'アミノ酸',
+  'コハク酸',
+  'アスパラギン酸',
+  'グルタミン酸'
+];
+const correct = 'グルタミン酸';
+
+document.getElementById('js-question').textContent = 'Question';
