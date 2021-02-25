7a5300edd9b531c7e9360301a8d0e6342ec11477
上田　剛史 <uedatsuyoshi@uedatakeshinoMacBook-Air.local>
2021-02-26 00:54:02 +0900

    Introduction of correctness judgment function
    # What
    正誤判定機能の導入。
    if文を使用する。また$button変数を作成し、コードの可読性を高める。
    
    # Why
    if文の条件分岐によりボタンを押されたときの正誤判定を可能にした。これによりクイズゲームとしての機能を成り立たせる。
    HTMLのオブジェクトを変数に代入するのでJavaScriptのルールに則り、buttonに$を付けている。

--- app.js
+++ app.js
@@ -9,12 +9,40 @@ const correct = 'グルタミン酸';
 
 //定数の文字列をHTMLに反映させる
 document.getElementById('js-question').textContent = question;
-document.getElementsByTagName('button')[0].textContent = answers[0];
-document.getElementsByTagName('button')[1].textContent = answers[1];
-document.getElementsByTagName('button')[2].textContent = answers[2];
-document.getElementsByTagName('button')[3].textContent = answers[3];
+
+const $button = document.getElementsByTagName('button');
+
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
 
 //ボタンをクリックしたら正誤判定
 document.getElementsByTagName('button')[0].addEventListener('click', () => {
-
+  if(correct === $button[0].textContent){
+    window.alert('正解!');
+  } else {
+    window.alert('不正解!');
+  }
+});
+document.getElementsByTagName('button')[1].addEventListener('click', () => {
+  if(correct === $button[1].textContent){
+    window.alert('正解!');
+  } else {
+    window.alert('不正解!');
+  }
+});
+document.getElementsByTagName('button')[2].addEventListener('click', () => {
+  if(correct === $button[2].textContent){
+    window.alert('正解!');
+  } else {
+    window.alert('不正解!');
+  }
+});
+document.getElementsByTagName('button')[3].addEventListener('click', () => {
+  if(correct === $button[3].textContent){
+    window.alert('正解!');
+  } else {
+    window.alert('不正解!');
+  }
 });
