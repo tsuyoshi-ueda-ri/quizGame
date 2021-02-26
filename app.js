7a655aca171d443133e4c88f6a4302dbc2e9003a
上田　剛史 <uedatsuyoshi@uedatakeshinoMacBook-Air.local>
2021-02-26 22:39:08 +0900

    Quiz game completed

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const quiz = [
 
 const quizLength = quiz.length
 let quizIndex = 0;
-
+let score = 0;
 
 const question = '三大旨味成分と言われているのはグアニル酸、イノシン酸、もう一つは次のうちどれか？';
 const answers = [
@@ -46,10 +46,10 @@ const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length; 
 //クイズの問題文、選択肢を定義
 const setupQuiz = () => {
-  document.getElementById('js-question').textContent = question;
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
   let buttonIndex = 0;
   while(buttonIndex < buttonLength) {
-    $button[buttonIndex].textContent = answers[buttonIndex];
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
     buttonIndex++;
   }
 }
@@ -57,10 +57,20 @@ const setupQuiz = () => {
 setupQuiz();
 
 const clickHandler = (e) => {
-  if(correct === e.target.textContent){
+  if(quiz[quizIndex].correct === e.target.textContent){
     window.alert('正解!');
+    score++;
   } else {
     window.alert('不正解!');
+  }
+  quizIndex++;
+
+  if(quizIndex < quizLength){
+    //問題数があればこちらを実行
+    setupQuiz();
+  } else {
+    //問題数が無ければこちらを実行
+    window.alert('終了！あなたの正解数は' + score + '/' + quizLength + 'です！');
   }
 };
 
